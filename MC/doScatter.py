e3b79d5b1b34e54742f5c37c0ee2493554ae1991
Elena Pascal <elena@pop-os.localdomain>
2020-01-23 16:22:14 -0500

    less of a disaster but listener is dead

--- MC/doScatter.py
+++ MC/doScatter.py
@@ -42,9 +42,6 @@ class MapScatterer(object):
         listen_func
             Function that listens to the queue
 
-        thingsToSave
-            A dictionary of data to save
-
         num_workers
             The numbers of workers to create in the pool.
             Default is the number of CPU available - 1
@@ -55,7 +52,7 @@ class MapScatterer(object):
         self.pool         = multiprocessing.Pool(num_workers)
 
         # set queue from manager
-        self.q            = {'electrons' : pathos_multiprocess.Manager().Queue(),
+        self.q_dict       = {'electrons' : pathos_multiprocess.Manager().Queue(),
                             'scatterings' : pathos_multiprocess.Manager().Queue()}
 
     def __call__(self, electrons, material, tables, thingsToSave, store):
@@ -68,26 +65,79 @@ class MapScatterer(object):
         '''
 
         # start listener
-        watcher = self.pool.apply_async(self.listener, (store, self.q))
-        print ('---------------defined watcher')
-
+        watcher = self.pool.apply_async(self.listener, store, self.q_dict)
+        print ('started listening ------------')
         # fire off workers for every electron
         jobs = []
 
         for el in electrons:
-            job = self.pool.apply_async(self.worker, (el, self.inputPar, material, tables, thingsToSave, self.q))
+            job = self.pool.apply_async(self.worker, args=(self.inputPar, material, tables, thingsToSave, self.q_dict))
             jobs.append(job)
-        print ('--------------fired off worker')
 
         # collect results from workers through the pool result quue
         for job in jobs:
             job.get()
-        print ('-----------------collected jobs')
+
         # kill the listener when done
-        self.q['electrons'].put('kill')
-        self.q['scatterings'].put('kill')
+        self.q_dict['electrons'].put('kill')
+        self.q_dict['scatterings'].put('kill')
         self.pool.close()
         self.pool.join()
+
+
+
+
+
+def listener(store, queue_dict):
+    '''
+    Listens for results in queue stores them
+    '''
+    print ('-----------inside listener')
+    results = {}
+    for key in queue_dict.keys():
+        results[key] = []
+
+    while True:
+        # get the results from queue
+        for key in queue_dict.keys():
+            results[key].append(pickle.loads(queue_dict[key].get()))
+            print ('---------results:', results)
+            # stop listening when kill signal arrives
+            if results[key] == 'kill':
+                break
+
+        # append the results to the store
+        writeBSEtoHDF5(results, store)
+
+
+def worker_DS(input, material, tables, thingsToSave, queue_dict):
+    '''
+    Scatter one electron using the direct model
+    and add the results to the queue
+    '''
+    print('---inside worker')
+    # initialise new electron
+    pos0 = np.array([0., 0., 0.,])
+    dir0 = np.array([-np.sin(np.radians(input['s_tilt'])),
+                    0., np.cos(np.radians(input['s_tilt']))])
+
+    # make an electron instance
+    el = electron(input['E0'], input['Emin'], pos0, dir0, thingsToSave)
+
+    # scatter this electron a full trajectory
+    trajectory_DS(el, material, input['Wc'], input['maxScatt'], tables)
+
+    # add results to the queue
+    try:
+        # make tuples out of dictionaries and pickle them and them add to the queue
+        queue_dict['electrons'].put(pickle.dumps(thingsToSave['el_output'].dict , protocol=2 ) )
+        queue_dict['scatterings'].put(pickle.dumps(thingsToSave['scat_output'].dict , protocol=2 ) )
+    except :
+        print ( "Unexpected error when pickling results:", sys.exc_info()[0])
+        raise
+
+        # flush info to terminal
+        #sys.stdout.flush()
 
 
 
@@ -144,121 +194,24 @@ if __name__ == '__main__': #this is necessary on Windows
     # read input parameters
     inputPar = readInput(inputFile)
 
+    # return some info about this run
+    print(' \n - Number of incident electrons per processor: %s' %inputPar['num_el'])
+    print(' \n - Material is: %s' %inputPar['material'])
+    print(' \n - Scattering mode is: %s \n' %inputPar['mode'])
 
-    print()
-    print(' number of incident electrons per processor:', inputPar['num_el'])
-    print()
-    print(' material is:', inputPar['material'])
-    print()
-    print(' scattering mode is:', inputPar['mode'])
-    print()
-
-    # compute integration tables
+    # for direct model we need integration tables
     if (inputPar['mode'] == 'DS'):
         # generate integration tables instances
         my_tables = genTables(inputPar)
 
-
-############################## units ###########################################
-    if use_units:
-        # Set all input parameters with units, make calculations @with_units
-        # and return pathlength and energy units
-        # With_units gets toggled on if input is of unit type
-        inputPar.update({'E0': UnitScalar(inputPar['E0'], units = 'eV')})
-        inputPar.update({'Emin': UnitScalar(inputPar['Emin'], units = 'eV')})
-        inputPar.update({'Wc': UnitScalar(inputPar['Wc'], units = 'eV')})
-
-        # update material parameters to unit type
-        thisMaterial.set_units()
-
-        # scatter one electron
-        pos0 = UnitArray((0., 0., 0.), units='angstrom')
-        dir0 = (-np.sin(np.radians(inputPar['s_tilt'])), 0., np.cos(np.radians(inputPar['s_tilt'])))
-        oneElectron = electron(inputPar['E0'], pos0, dir0)
-
-        if (inputPar['mode'] == 'DS'):
-            scatterOneEl_DS_wUnits(oneElectron, thisMaterial, inputPar['Emin'], inputPar['Wc'],  tables_moller, tables_gryz)
-            print ('- Energy units:', oneElectron.energy.units)
-            print ('- Distance units:', oneElectron.xyz.units)
-            sys.exit()
-
-        elif (inputPar['mode'] == 'cont'):
-            if (inputPar['Bethe_model'] == 'classical'):
-                scatterOneEl_cont_cl_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
-            elif (inputPar['Bethe_model'] == 'JL'):
-                scatterOneEl_cont_JL_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
-            elif (inputPar['Bethe_model'] == 'explicit'):
-                scatterOneEl_cont_expl_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
-            else:
-                print (' ! I did not understand the Bethe model type in units check')
-                print (' ! Exiting')
-                sys.exit()
-
-            print()
-            print ('- Energy units:', oneElectron.energy.units)
-            print ('- Distance units:', oneElectron.xyz.units)
-            sys.exit()
-        else:
-            print()
-            print (' I did not understand the input scattering mode')
-
-###############################################################################
-
-    def worker_DS(el_id, input, material, tables, thingsToSave, queue):
-        '''
-        Scatter one electron using the direct model
-        and add the results to the queue
-        '''
-        # initialise new electron
-        pos0 = np.array([0., 0., 0.,])
-        dir0 = np.array([-np.sin(np.radians(input['s_tilt'])),
-                        0., np.cos(np.radians(input['s_tilt']))])
-
-        # make an electron instance
-        el = electron(input['E0'], input['Emin'], pos0, dir0, thingsToSave)
-        print ('----------------made electron')
-        # scatter this electron a full trajectory
-        trajectory_DS(el, material, input['Wc'], input['maxScatt'], tables)
-        print  ('-----------did one trajectory')
-        # add results to the queue
-        try:
-            # make tuples out of dictionaries and pickle them and them add to the queue
-            queue['electrons'].put(pickle.dumps(thingsToSave['el_output'].dict , protocol=2 ) )
-            queue['scatterings'].put(pickle.dumps(thingsToSave['scat_output'].dict , protocol=2 ) )
-        except :
-            print ( "Unexpected error when pickling results:", sys.exc_info()[0])
-            raise
-
-            # flush info to terminal
-            #sys.stdout.flush()
-
-
     # name the hdf file that stores the results
     storeFile = '../data/Al_BSE' +   '_mode:' + str(inputPar['mode'])  +\
-                                '_tilt:' + str(inputPar['s_tilt'])+\
-                                '_Emin:' + str(inputPar['Emin'])  +\
-                                '_E0:'   + str(inputPar['E0'])    +\
-                                '_tolE:' + str(inputPar['tol_E']) +\
-                                '_tolW:' + str(inputPar['tol_W']) +\
+                                '_tilt:' + str(int(inputPar['s_tilt']))+\
+                                '_Emin:' + str(int(inputPar['Emin']))  +\
+                                '_E0:'   + str(int(inputPar['E0']))    +\
+                                '_tolE:' + str(inputPar['tol_E'])      +\
+                                '_tolW:' + str(inputPar['tol_W'])      +\
                                 '.h5'
-
-    print ('-----------------------instance done')
-
-    def listener(store, queue):
-        '''
-        Listens for results in queue stores them
-        '''
-
-        while True:
-            # get the results from queue
-            results = queue.get()
-            print ('results:', results)
-            # stop listening when kill signal arrives
-            if results == 'kill':
-                break
-
-            # append the results to the store
-            writeBSEtoHDF5(results, store)
 
 
     # create HDF5 store
@@ -267,16 +220,25 @@ if __name__ == '__main__': #this is necessary on Windows
     # write input parameters to pandas series -> transposed dataframe
     store['input'] = pd.DataFrame(pd.Series(inputPar)).T
 
+    # set the material
     target_material = material(inputPar['material'])
 
+    # make a disctionary with parameters to save
     whatToSave = {'el_output': thingsToSave(inputPar['electron_output']),
                  'scat_output': thingsToSave(inputPar['scatter_output']) }
 
-    scatter = MapScatterer(inputPar, worker_DS, listener, 3)
-    print ('\n instance done------')
-    scatter([el for el in range(10)], target_material, my_tables, whatToSave, store)
+    # instace of scatter mapper
+    scatter = MapScatterer(inputPar, worker_DS, listener, num_workers=3)
 
+    # scatter object sent to multithreading
+    scatter([el for el in range(5)], target_material, my_tables, whatToSave, store)
+
+    store.close()
     print (' BSE data had been written to ', storeFile)
+
+
+
+
 
     # # number of processes available
     # num_proc = cpu_count()-1 # leave one cpu thread free for user
@@ -335,3 +297,50 @@ if __name__ == '__main__': #this is necessary on Windows
     # writeBSEtoHDF5(results, inputPar, fileBSE)
     # print()
     # print (' time spent writting to file:', time.time()-time_start)
+
+
+
+############################## units ###########################################
+    # if use_units:
+    #     # Set all input parameters with units, make calculations @with_units
+    #     # and return pathlength and energy units
+    #     # With_units gets toggled on if input is of unit type
+    #     inputPar.update({'E0': UnitScalar(inputPar['E0'], units = 'eV')})
+    #     inputPar.update({'Emin': UnitScalar(inputPar['Emin'], units = 'eV')})
+    #     inputPar.update({'Wc': UnitScalar(inputPar['Wc'], units = 'eV')})
+    #
+    #     # update material parameters to unit type
+    #     thisMaterial.set_units()
+    #
+    #     # scatter one electron
+    #     pos0 = UnitArray((0., 0., 0.), units='angstrom')
+    #     dir0 = (-np.sin(np.radians(inputPar['s_tilt'])), 0., np.cos(np.radians(inputPar['s_tilt'])))
+    #     oneElectron = electron(inputPar['E0'], pos0, dir0)
+    #
+    #     if (inputPar['mode'] == 'DS'):
+    #         scatterOneEl_DS_wUnits(oneElectron, thisMaterial, inputPar['Emin'], inputPar['Wc'],  tables_moller, tables_gryz)
+    #         print ('- Energy units:', oneElectron.energy.units)
+    #         print ('- Distance units:', oneElectron.xyz.units)
+    #         sys.exit()
+    #
+    #     elif (inputPar['mode'] == 'cont'):
+    #         if (inputPar['Bethe_model'] == 'classical'):
+    #             scatterOneEl_cont_cl_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
+    #         elif (inputPar['Bethe_model'] == 'JL'):
+    #             scatterOneEl_cont_JL_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
+    #         elif (inputPar['Bethe_model'] == 'explicit'):
+    #             scatterOneEl_cont_expl_wUnits(oneElectron, thisMaterial, inputPar['Emin'])
+    #         else:
+    #             print (' ! I did not understand the Bethe model type in units check')
+    #             print (' ! Exiting')
+    #             sys.exit()
+    #
+    #         print()
+    #         print ('- Energy units:', oneElectron.energy.units)
+    #         print ('- Distance units:', oneElectron.xyz.units)
+    #         sys.exit()
+    #     else:
+    #         print()
+    #         print (' I did not understand the input scattering mode')
+
+###############################################################################
